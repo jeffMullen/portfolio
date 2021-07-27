926e9741953c33ae513d9689552045f0437c87ea
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-07-27 18:16:16 -0400

    dynamically add application links to modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,31 @@
 let applications = $('.applications-content');
+const modalBody = $('.modal-body');
 
 const linkModal = imageLink => {
     console.log(imageLink);
+    // console.log(imageLink.previousElementSibling.previousElementSibling.textContent);
 
+    let liveLink = imageLink.previousElementSibling.previousElementSibling.textContent;
+    let gitHubLink = imageLink.previousElementSibling.textContent;
+
+
+
+    let liveApplication = $('<a>');
+    liveApplication.attr('href', `${liveLink}`);
+    liveApplication.text('Live Application');
+
+    let gitHubApplication = $('<a>');
+    gitHubApplication.attr('href', `${gitHubLink}`);
+    gitHubApplication.text('GitHub Application');
+
+    modalBody.append(liveApplication);
+    modalBody.append(gitHubApplication);
 
 }
 
 applications.on('click', 'img', function () {
     let imageLink = this;
+
     linkModal(imageLink);
     console.log('click');
 })
